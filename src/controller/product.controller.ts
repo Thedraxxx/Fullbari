7c10547a3bca629aa48b4bc10c82bffaea583c6b
Roshan Gurung <roshc10grg@gmail.com>
2025-06-15 14:13:52 +0545

    update image url in controller

--- src/controller/product.controller.ts
+++ src/controller/product.controller.ts
@@ -4,10 +4,11 @@ import { productValidate } from "../Schema/product.schema";
 import { createProductService } from "../services/product.service";
 import apiResponse from "../utils/apiResponse";
 import apiError from "utils/apiErrors";
+import uploadOnCloudnary from "utils/cloudnary";
 const insertProduct = asyncHandler(async (req: Request, res: Response) => {
   const validProduct = productValidate.parse(req.body);
   
-   const image = req.file;
+   const image = req.file?.path;
   if(!image){
     throw new apiError(401,"Product Image is required")
   }
@@ -16,7 +17,7 @@ const insertProduct = asyncHandler(async (req: Request, res: Response) => {
   if(!imageURL){
     throw new apiError(401,"Failed to upload on the cloudnary.")
   }
-   
+   validProduct.productImage = [imageURL];
 
   const productData = await createProductService(validProduct);
   return res
