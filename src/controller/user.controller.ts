e5e14f4ad25094aa6252b2cd95bc909e9681e400
Roshan Gurung <roshc10grg@gmail.com>
2025-06-22 08:32:05 +0545

    user logout updated

--- src/controller/user.controller.ts
+++ src/controller/user.controller.ts
@@ -1,9 +1,9 @@
-
 import asyncHandler from "../utils/asyncHandler";
 import { Request, Response, CookieOptions } from "express";
 import { register, login } from "../services/user.service";
 import apiResponse from "../utils/apiResponse";
 import { registerValidate, loginValidate } from "../Schema/user.schema";
+import User from "../model/user.model";
 
 const userRegister = asyncHandler(async (req: Request, res: Response) => {
   const validateRegisterData = registerValidate.parse(req.body);
@@ -22,9 +22,34 @@ const userLogin = asyncHandler(async (req: Request, res: Response) => {
     .status(200)
     .cookie("accessToken", accessToken, options)
     .cookie("refreshToken", refreshToken, options)
-    .json(new apiResponse(200,user,"user logged in successfylly"));
+    .json(new apiResponse(200, user, "user logged in successfylly"));
 });
-const userLogout = asyncHandler(async (req: Request,res: Response)=>{
-    
-})
-export { userRegister, userLogin };
+const userLogout = asyncHandler(async (req: Request, res: Response) => {
+  //user la logout thixhxa
+  //endpoint hit garxa .... ani jwtverify garxa ....
+  // ani logout controller ma chi User khojxa ra rafreshToken lai unset garxa
+  //and res ma cookie clear ganey both at and rt
+  await User.findByIdAndUpdate(
+    req.user?.id,
+    {
+      // maila refreshtoken lai save garya xu so rt matra delete garnu paryo db bata
+      $unset: {
+        refreshToken: 1,
+      },
+    },
+    {
+      new: true,//return updated document 
+    }
+  );
+
+  const options: CookieOptions = {
+    httpOnly: true,
+    secure: true,
+  };
+  return res
+    .status(200)
+    .clearCookie("refreshToken", options)
+    .clearCookie("accessToken", options)
+    .json(new apiResponse(200, {}, "User logged out successfully"));
+});
+export { userRegister, userLogin, userLogout };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -1,11 +1,11 @@
 import { userRegister,userLogin,userLogout } from "../controller/user.controller";
-import jwtVerify from "middleware/auth.middleware";
+import jwtVerify from "../middleware/auth.middleware";
 import { Router } from "express";
 
 const userRouter = Router();
 
 userRouter.route("/register").post(userRegister);
 userRouter.route("/login").post(userLogin);
-userRouter.route("/logout").post(jwtVerify,userLogout)
+userRouter.route("/logout").delete(jwtVerify,userLogout)
 
 export default userRouter;
