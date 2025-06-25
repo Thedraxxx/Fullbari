6e67da76d3a629ab620263740e905a310b526c56
Roshan Gurung <roshc10grg@gmail.com>
2025-06-25 12:17:36 +0545

    create category service

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -1,9 +1,19 @@
+import Category from "model/category.model";
 import { ICategory } from "../Schema/category.schema";
+import slugify from "slugify";
 
 
 const createCategoryService = async(data: ICategory)=>
 {
-     
+       const {categoryName, discription} = data;
+       const slug = slugify(categoryName,{lower: true});
+        const category= await Category.create({
+            categoryName,
+            slug,
+            discription
+          });
+        return category;
+
 }
 
 export {createCategoryService}
