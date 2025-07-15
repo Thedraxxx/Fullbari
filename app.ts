7bca42ec61b9ca6f60a70ea622fc1d7d43b60d92
Roshan Gurung <roshc10grg@gmail.com>
2025-07-15 12:48:57 +0545

    cors setup for bacekdn and frotnend connection

--- app.ts
+++ app.ts
@@ -3,8 +3,11 @@ import errorHandler from "./src/middleware/errorHandler";
 import userRouter from "./src/routes/user.routes";
 import { productRouter } from "./src/routes/product.routes";
 import categoryRouter from "./src/routes/category.routes";
+import cors from "cors"
 const app = express();
-
+app.use(cors({
+    origin: "*"
+}))
 app.use(express.json({limit: "1mb"}));
 app.use("/api/v2/users",userRouter);
 app.use("/api/v2/products",productRouter);
